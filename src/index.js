c21dae7ad3e8ff0ad4830390ae91e3e969b4b1d8
Chitransh27309-crypto <ayush27309@gmail.com>
2026-06-23 15:08:51 +0530

    connect to mongoATLAS through new folder

--- src/index.js
+++ src/index.js
@@ -1,23 +1,8 @@
-import dotenv from "dotenv";
-dotenv.config()
-import mongoose from "mongoose";
-import {DB_NAME} from "./constant.js"
+import dotenv from "dotenv"
+dotenv.config({
+    path: "./.env"
+})
+import ConnectDB from "./db/index.js";
 
-import express from "express";
-const app = express();
 
-( async ()=>{
-    try {
-       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-       app.on("error",(error)=>{
-        console.log("ERROR: ",error)
-        throw error;
-       })
-
-       app.listen(process.env.PORT,()=>{
-        console.log(`App is listening on port ${process.env.PORT}`)
-       })
-    } catch (error) {
-        console.log("ERROR: ",error)
-    }
-})()
+ConnectDB();
