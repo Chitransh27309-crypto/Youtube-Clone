85b26237192219d864f18557b5543ba7a3893725
Chitransh27309-crypto <ayush27309@gmail.com>
2026-06-23 14:45:25 +0530

    Fixing error coming in connecting DB directly through index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+import dotenv from "dotenv";
+dotenv.config()
 import mongoose from "mongoose";
-import {DB_NAME} from "./constant"
+import {DB_NAME} from "./constant.js"
 
 import express from "express";
 const app = express();
